ea3595aee8556f0bd20c43b9dcab46652aa2ef2a
Akatsuki <103162172+ArctynFox@users.noreply.github.com>
2024-04-16 14:57:05 -0500

    Fixed incorrect app version in build.gradle

--- app/src/main/java/com/wkuxr/sunsketcher/App.java
+++ app/src/main/java/com/wkuxr/sunsketcher/App.java
@@ -3,6 +3,7 @@ package com.wkuxr.sunsketcher;
 import android.app.Application;
 import android.content.Context;
 
+//App class that just statically holds a singleton for the current activity Context
 public class App extends Application {
     private static Context context;
     public static Context getContext(){
